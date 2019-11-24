25e3aceefb499603e5798198b9e5e9cf615b9303
Siddharth <4932489-sid-7@users.noreply.gitlab.com>
2019-11-24 20:46:35 +0530

    update modules

--- InstructionRegister.v
+++ InstructionRegister.v
@@ -2,16 +2,16 @@
 
 module InstructionRegister(
     input             CLK,
-    input             L_IR,
+    //input             L_IR,
     input       [7:0] PM_in,
     output reg  [7:0] OC_out
     );
     
     initial     OC_out = 8'h00;
 
-    always @(*)
+    always @(posedge CLK)
     begin
-        if(L_IR)
-            OC_out = PM_in;
+    //    if(L_IR)
+        OC_out = PM_in;
     end
 endmodule
--- mem_prog.v
+++ mem_prog.v
@@ -9,6 +9,7 @@ reg [15:0] progMem[255:0];
 
 initial begin
     //$readmemh("/Users/sid/Code/Verilog/Pipeline-1/program.txt",progMem);
+    //$readmemh("program.txt",progMem);
 end
 
 assign {opcode, operand} = progMem[address];
